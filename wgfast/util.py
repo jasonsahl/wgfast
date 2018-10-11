ab8756c8081a4c0fa6ca199e6eef7771a37c93c7
Tara Furstenau <tara.furstenau@nau.edu>
2018-10-11 11:31:20 -0700

    Update util.py

--- wgfast/util.py
+++ wgfast/util.py
@@ -279,7 +279,6 @@ def get_sequence_length(fastq_in):
 
 def run_bwa_dev(read_1, read_2, processors, name, reference):
     #read_group = '@RG\tID:%s\tSM:vac6wt\tPL:ILLUMINA\tPU:vac6wt' % name
-    print(os.getcwd())
     other_opts = ["|","samtools","view","-uS","-","|","samtools","sort","-@","4","-o","%s_renamed.bam" % name]
     bwa_dev(reference,read_1,read_2,processors,other_opts,name)
 
